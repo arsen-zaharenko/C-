85f43727bebaf9c94702ec4f3fcd32de5fe56a14
arsen-zaharenko <59254411+arsen-zaharenko@users.noreply.github.com>
2020-11-14 15:01:31 +0300

    Update Transport.h

--- lab_5/Transport.h
+++ lab_5/Transport.h
@@ -1,22 +1,56 @@
+#include <iostream> 
+
 class Transport {
-protected:
-    int speed = 50;
-    int cost = 20;
 public:
-    virtual void getData() = 0;
+    virtual void showTime() = 0;
+    virtual void showCost() = 0;
+    virtual void setTime(double) = 0;
+    virtual void setCost(int) = 0;
+    virtual double getTime() = 0;
+    virtual int getCost() = 0;
 };
 
 class Auto : public Transport {
+    double time;
+    int cost;
 public:
-    void getData();
+    Auto(double time_, int cost_) : time(time_), cost(cost_) {};
+    void showTime();
+    void showCost();
+    void setTime(double);
+    void setCost(int);
+    double getTime();
+    int getCost();
+    friend std::ostream& operator<< (std::ostream&, Auto*);
+    friend std::istream& operator>> (std::istream&, Auto*);
 };
 
 class Bicycle : public Transport {
+    double time;
+    int cost;
 public:
-    void getData();
+    Bicycle(double time_, int cost_) : time(time_), cost(cost_) {};
+    void showTime();
+    void showCost();
+    void setTime(double);
+    void setCost(int);
+    double getTime();
+    int getCost();
+    friend std::ostream& operator<< (std::ostream&, Bicycle*);
+    friend std::istream& operator>> (std::istream&, Bicycle*);
 };
 
 class Cart : public Transport {
+    double time;
+    int cost;
 public:
-    void getData();
+    Cart(double time_, int cost_) : time(time_), cost(cost_) {};
+    void showTime();
+    void showCost();
+    void setTime(double);
+    void setCost(int);
+    double getTime();
+    int getCost();
+    friend std::ostream& operator<< (std::ostream&, Cart*);
+    friend std::istream& operator>> (std::istream&, Cart*);
 };
